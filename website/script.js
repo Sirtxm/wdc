c7ebc29d51ea4d8333980d83a98cbaab0a2bc0f1
Sirtxm <teeraphat.stamp@gmail.com>
2024-07-08 15:21:49 +0700

    motto add

--- website/script.js
+++ website/script.js
@@ -1,53 +1,54 @@
-// parallax scrolling
-document.addEventListener('DOMContentLoaded', () => {
-    let text = document.getElementById('text');
-    let footer = document.querySelector('footer');
-    
-    window.addEventListener('scroll', () => {
-        let value = window.scrollY;
-        let maxScroll = footer.offsetTop - window.innerHeight;
-
-        if (value * 2.5 <= maxScroll) {
-            text.style.marginTop = `${value * 2.5}px`;
-        } else {
-            text.style.marginTop = `${maxScroll}px`;
-        }
-    });
-});
-
-// wake_cat
+ // parallax scrolling
 {
-    const wakecat = document.querySelector("#wake_cat");
-    const btn = document.querySelector("#btn");
-    const counter = document.querySelector("#counter");
-
-    const wakeImg = "/source/datapop//wake.png";
-    const sleepImg = "/source/datapop/sleep.png";
-
-    btn.addEventListener("mousedown", wake);
-    btn.addEventListener("mouseup", sleep);
-
-    let clickCount = 0;
-    btn.addEventListener("touchstart", function(e) {
-        e.preventDefault();
-        wake()
-    })
-
-    btn.addEventListener("touchend", function(e) {
-        e.preventDefault();
-        sleep()
-    })
-
+    document.addEventListener('DOMContentLoaded', () => {
+        let text = document.getElementById('text');
+        let footer = document.querySelector('footer');
+        
+        window.addEventListener('scroll', () => {
+            let value = window.scrollY;
+            let maxScroll = footer.offsetTop - window.innerHeight;
     
-    function wake() {
-        wakecat.src = wakeImg;
-        incrementCounter();
+            if (value * 2.5 <= maxScroll) {
+                text.style.marginTop = `${value * 2.5}px`;
+            } else {
+                text.style.marginTop = `${maxScroll}px`;
+            }
+        });
+    });}
+    
+    //  wake_cat
+    {
+        const wakecat = document.querySelector("#wake_cat");
+        const btn = document.querySelector("#btn");
+        const counter = document.querySelector("#counter");
+    
+        const wakeImg = "/source/datapop//wake.png";
+        const sleepImg = "/source/datapop/sleep.png";
+    
+        btn.addEventListener("mousedown", wake);
+        btn.addEventListener("mouseup", sleep);
+    
+        let clickCount = 0;
+        btn.addEventListener("touchstart", function(e) {
+            e.preventDefault();
+            wake()
+        })
+    
+        btn.addEventListener("touchend", function(e) {
+            e.preventDefault();
+            sleep()
+        })
+    
+        
+        function wake() {
+            wakecat.src = wakeImg;
+            incrementCounter();
+        }
+        function sleep() {
+            wakecat.src = sleepImg;
+        }
+        function incrementCounter() {
+            clickCount++;
+            counter.textContent = clickCount;
+        }
     }
-    function sleep() {
-        wakecat.src = sleepImg;
-    }
-    function incrementCounter() {
-        clickCount++;
-        counter.textContent = clickCount;
-    }
-}
